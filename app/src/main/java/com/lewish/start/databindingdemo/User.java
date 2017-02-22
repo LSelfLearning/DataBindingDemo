69c9a88a80f5b5d5e20b55c213b3e1c478a4ef2b
sundong <sundong@yingu.com>
2017-02-22 17:55:05 +0800

    下班前备份

--- app/src/main/java/com/lewish/start/databindingdemo/User.java
+++ app/src/main/java/com/lewish/start/databindingdemo/User.java
@@ -1,10 +1,14 @@
 package com.lewish.start.databindingdemo;
 
+import android.databinding.BaseObservable;
+import android.databinding.Bindable;
+
+
 /**
  * Created by Administrator on 2017/2/22 11:33.
  */
 
-public class User {
+public class User extends BaseObservable{
     private String firstName;
     private String lastName;
 
@@ -12,21 +16,23 @@ public class User {
         this.firstName = firstName;
         this.lastName = lastName;
     }
-
+    @Bindable
     public String getFirstName() {
         return firstName;
     }
 
     public void setFirstName(String firstName) {
         this.firstName = firstName;
+        notifyPropertyChanged(BR.firstName);
     }
-
+    @Bindable
     public String getLastName() {
         return lastName;
     }
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+        notifyPropertyChanged(BR.lastName);
     }
 
 }
